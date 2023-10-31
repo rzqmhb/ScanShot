63424c24090a76c1fc9782812a17e96216d85390
Raden616Rizki <rizkiraden616@gmail.com>
2023-10-31 12:33:03 +0700

    Commit Result Page

--- service/ScanShot/lib/pages/dashboard.dart
+++ service/ScanShot/lib/pages/dashboard.dart
@@ -15,17 +15,56 @@ class DashboardPageState extends State<DashboardPage> {
     KartuKeluarga(
       id: 1,
       gambar: 'assets/sample_kk.jpg',
-      teks: 'Keluarga Cemara',
+      teks: '''Keluarga Keluarga
+      Nomor : 7308180205083847
+      Kepala Keluarga : ***
+      Alamat : ****
+      Anggota Keluarga :
+      *** :
+      NIK : ***
+      Kelamin :
+      TTL :
+      Agama :
+      Pendidikan :
+      Pekerjaan :
+      ***
+      ***''',
     ),
     KartuKeluarga(
       id: 2,
       gambar: 'assets/sample_kk.jpg',
-      teks: 'Keluarga Cemara',
+      teks: '''Keluarga Keluarga
+      Nomor : 7308180205083848
+      Kepala Keluarga : ***
+      Alamat : ****
+      Anggota Keluarga :
+      *** :
+      NIK : ***
+      Kelamin :
+      TTL :
+      Agama :
+      Pendidikan :
+      Pekerjaan :
+      ***
+      ***''',
     ),
     KartuKeluarga(
       id: 3,
       gambar: 'assets/sample_kk.jpg',
-      teks: 'Keluarga Cemara',
+      teks: '''Keluarga Keluarga
+      Nomor : 7308180205083867
+      Kepala Keluarga : ***
+      Alamat : ****
+      Anggota Keluarga :
+      *** :
+      NIK : ***
+      Kelamin :
+      TTL :
+      Agama :
+      Pendidikan :
+      Pekerjaan :
+      ***
+      ***''',
     ),
   ];
 
@@ -47,32 +86,36 @@ class DashboardPageState extends State<DashboardPage> {
             top: 92,
             left: 26,
             right: 26,
-            bottom: 125,
+            bottom: 72,
             child: ListView.builder(
               itemCount: kk.length,
               itemBuilder: (context, index) {
                 final kartuKeluarga = kk[index];
                 return InkWell(
                   onTap: () {
-                    Navigator.pushNamed(context, '/result', arguments: kk);
+                    Navigator.pushNamed(context, '/result',
+                        arguments: kartuKeluarga);
                   },
                   child: Container(
                     height: 100,
                     margin: const EdgeInsets.only(bottom: 26),
                     child: Row(
                       children: [
-                        Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFFFFFFF),
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              kartuKeluarga.gambar,
-                              fit: BoxFit.cover,
+                        Hero(
+                          tag: 'gambarKK-${kartuKeluarga.id}',
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFFFFFFF),
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                kartuKeluarga.gambar,
+                                fit: BoxFit.cover,
+                              ),
                             ),
                           ),
                         ),
@@ -81,19 +124,22 @@ class DashboardPageState extends State<DashboardPage> {
                           child: Stack(
                             alignment: Alignment.bottomRight,
                             children: [
-                              Container(
-                                height: 100,
-                                decoration: BoxDecoration(
-                                  color: const Color(0xFFFFFFFF),
-                                  borderRadius: BorderRadius.circular(8.0),
-                                ),
-                                alignment: Alignment.center,
-                                child: SelectableText(
-                                  kartuKeluarga.teks,
-                                  style: const TextStyle(
-                                    color: Color(0xFF252525),
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
+                              Hero(
+                                tag: 'textKK-${kartuKeluarga.id}',
+                                child: Container(
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFFFFFFFF),
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: SelectableText(
+                                    kartuKeluarga.teks,
+                                    style: const TextStyle(
+                                      color: Color(0xFF252525),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
                                   ),
                                 ),
                               ),
