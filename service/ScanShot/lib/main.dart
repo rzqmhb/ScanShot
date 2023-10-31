ac16036dd3943bb6309cd08662b50dc69c4d2095
Amaliastf <125347989+Amaliastf@users.noreply.github.com>
2023-10-31 13:16:42 +0700

    login pages

--- service/ScanShot/lib/main.dart
+++ service/ScanShot/lib/main.dart
@@ -7,18 +7,20 @@ import 'package:scanshot/pages/scan.dart';
 import 'package:scanshot/pages/scanPreview.dart';
 import 'package:scanshot/pages/dashboard.dart';
 import 'package:scanshot/pages/result.dart';
+import 'package:scanshot/pages/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
-      '/': (context) => const DashboardPage(),
+      '/dashboard': (context) => const DashboardPage(),
       '/profile': (context) => ProfilePage(),
       '/help': (context) => HelpPage(),
       '/result': (context) => const ResultPage(),
       '/scanPreview': (context) => ScanPreview(),
       '/scan': (context) => Scan(),
       '/register': (context) => RegisterPage(),
+      '/': (context) => LoginPage(), // Halaman login
     },
   ));
 }
