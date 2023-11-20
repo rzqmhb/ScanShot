98aa34168671f4d8b496d6d92c582747bc27e989
DioGitH <maulidiobisnis16@gmail.com>
2023-11-20 14:40:11 +0700

    feat: add widget

--- service/ScanShot/lib/pages/register.dart
+++ service/ScanShot/lib/pages/register.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:scanshot/widget/checkBox.dart';
+import 'package:scanshot/widget/loading.dart';
+import 'package:scanshot/widget/textField.dart';
 
 class RegisterPage extends StatefulWidget {
   RegisterPage({super.key});
@@ -16,110 +18,30 @@ class _RegisterPageState extends State<RegisterPage> {
     height: 200,
   );
 
-  Widget textField({required String hintText}) {
+  Widget textLogin(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-      child: TextField(
-        style: TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),
-            fontSize: 12,
-            fontWeight: FontWeight.w400),
-        decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            borderSide: BorderSide(
-                color: Color.fromARGB(255, 255, 198, 11), width: 2.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            borderSide: BorderSide(
-                color: Color.fromARGB(255, 255, 198, 11), width: 2.0),
-          ),
-          contentPadding:
-              EdgeInsets.symmetric(horizontal: 20.0, vertical: 12.0),
-          hintText: hintText,
-          hintStyle: TextStyle(
-              color: Color.fromARGB(255, 255, 255, 255),
-              fontSize: 12,
-              fontWeight: FontWeight.w400),
-        ),
-      ),
-    );
-  }
-
-  Widget checkBox() {
-    return Padding(
-      padding: EdgeInsets.only(top: 10, bottom: 7),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      padding: EdgeInsets.only(top: 10),
+      child: Column(
         children: [
-          Container(
-            child: CheckBoxWidget(),
-          ),
           Text(
-            'Saya telah menyetujui ',
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.w400, fontSize: 11),
+            'Sudah punya akun?',
+            style: TextStyle(color: Colors.white),
           ),
-          Text(
-            'syarat & ketentuan',
-            style: TextStyle(
-                color: Colors.blue[200],
-                decoration: TextDecoration.underline,
-                fontWeight: FontWeight.w400,
-                fontSize: 11),
+          InkWell(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              'Login disini',
+              style: TextStyle(color: Color.fromARGB(255, 255, 198, 11)),
+            ),
           )
         ],
       ),
     );
   }
 
-  Widget textLogin(BuildContext context) {
-  return Padding(
-    padding: EdgeInsets.only(top: 10),
-    child: Column(
-      children: [
-        Text(
-          'Sudah punya akun?',
-          style: TextStyle(color: Colors.white),
-        ),
-        InkWell(
-          onTap: () {
-            Navigator.of(context).pop();
-          },
-          child: Text(
-            'Login disini',
-            style: TextStyle(color: Color.fromARGB(255, 255, 198, 11)),
-          ),
-        )
-      ],
-    ),
-  );
-  }
-
   bool isLoading = false;
-  Widget loading() {
-    return Stack(
-      children: [
-        RegisterPage(),
-        Positioned.fill(
-            child: Center(
-          child: Container(
-              padding: EdgeInsets.all(8.0),
-              width: 150,
-              height: 120,
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              child: LoadingAnimationWidget.staggeredDotsWave(
-                  color: Color.fromARGB(255, 255, 198, 11), size: 100)),
-        )),
-      ],
-      // child:
-    );
-  }
 
   Widget buttonRegister(BuildContext context) {
     return Padding(
@@ -153,7 +75,7 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return isLoading
-        ? loading()
+        ? LoadingWidget(widget: RegisterPage())
         : MaterialApp(
             title: 'Flutter layout demo',
             home: Scaffold(
@@ -171,11 +93,11 @@ class _RegisterPageState extends State<RegisterPage> {
                     // mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       icon,
-                      textField(hintText: 'Nama Pengguna'),
-                      textField(hintText: 'Surel'),
-                      textField(hintText: 'Kata Sandi'),
-                      textField(hintText: 'Konfirmasi Kata Sandi'),
-                      checkBox(),
+                      TextFieldWidget(hintText: 'Nama Pengguna'),
+                      TextFieldWidget(hintText: 'Surel'),
+                      TextFieldWidget(hintText: 'Kata Sandi'),
+                      TextFieldWidget(hintText: 'Konfirmasi Kata Sandi'),
+                      CheckBoxWidget(),
                       buttonRegister(context),
                       textLogin(context),
                     ],
@@ -186,18 +108,3 @@ class _RegisterPageState extends State<RegisterPage> {
           );
   }
 }
-
-// class loading extends StatelessWidget {
-//   const loading({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//             child:
-//                 LoadingAnimationWidget.waveDots(color: Colors.blue, size: 100)),
-//       ),
-//     );
-//   }
-// }
